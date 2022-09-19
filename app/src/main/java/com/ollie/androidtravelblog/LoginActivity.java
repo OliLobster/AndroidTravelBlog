083af03626b5d00eacaaa4c4b4a9f43e63921692
OliLobster <sgyr1993@163.com>
2022-09-19 22:49:19 +0800

    view binding for login Activity

--- app/src/main/java/com/ollie/androidtravelblog/LoginActivity.java
+++ app/src/main/java/com/ollie/androidtravelblog/LoginActivity.java
@@ -3,12 +3,23 @@ package com.ollie.androidtravelblog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextInputLayout textUsernameLayout;
+    private TextInputLayout textPasswordInput;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textUsernameLayout = findViewById(R.id.textUsernameLayout);
+        textPasswordInput = findViewById(R.id.textPasswordInput);
+        loginButton = findViewById(R.id.loginButton);
+
     }
 }
