8623379137de16a4717e544c9c5bcf5063419d89
OliLobster <sgyr1993@163.com>
2022-10-07 00:20:23 +0800

    animate image loading

--- app/src/main/java/com/ollie/androidtravelblog/BlogDetailsActivity.java
+++ app/src/main/java/com/ollie/androidtravelblog/BlogDetailsActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class BlogDetailsActivity extends AppCompatActivity {
 
@@ -27,12 +28,14 @@ public class BlogDetailsActivity extends AppCompatActivity {
         ImageView imageMain = findViewById(R.id.imageMain);
         Glide.with(this)
                 .load(IMAGE_URL)
+                .transition(DrawableTransitionOptions.withCrossFade())
                 .into(imageMain);
 
         ImageView imageAvatar = findViewById(R.id.imageAvatar);
         Glide.with(this)
                 .load(AVATAR_URL)
                 .transform(new CircleCrop())
+                .transition(DrawableTransitionOptions.withCrossFade())
                 .into(imageAvatar);
 
         TextView textTitle = findViewById(R.id.textTitle);
