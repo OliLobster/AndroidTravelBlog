ee016e329d08ff42d6f1262dd14848da26803b1b
OliLobster <sgyr1993@163.com>
2022-10-06 20:50:18 +0800

    setting data from code

--- app/src/main/java/com/ollie/androidtravelblog/BlogDetailsActivity.java
+++ app/src/main/java/com/ollie/androidtravelblog/BlogDetailsActivity.java
@@ -1,6 +1,9 @@
 package com.ollie.androidtravelblog;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,32 @@ public class BlogDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_details);
+
+        ImageView imageMain = findViewById(R.id.imageMain);
+        imageMain.setImageResource(R.drawable.sydney_image);
+
+        ImageView imageAvatar = findViewById(R.id.imageAvatar);
+        imageAvatar.setImageResource(R.drawable.avatar);
+
+        TextView textTitle = findViewById(R.id.textTitle);
+        textTitle.setText("G'day from Sydney");
+
+        TextView textDate = findViewById(R.id.textDate);
+        textDate.setText("August 2, 2019");
+
+        TextView textAuthor = findViewById(R.id.textAuthor);
+        textAuthor.setText("Grayson Wells");
+
+        TextView textRating = findViewById(R.id.textRating);
+        textRating.setText("4.4");
+
+        TextView textViews = findViewById(R.id.textViews);
+        textViews.setText("(2687 views)");
+
+        TextView textDescription = findViewById(R.id.textDescription);
+        textDescription.setText("Australia is one of the most popular travel destinations in the world.");
+
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        ratingBar.setRating(4.4f);
     }
 }
