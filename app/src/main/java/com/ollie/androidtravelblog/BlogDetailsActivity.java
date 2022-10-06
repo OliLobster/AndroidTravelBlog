3bb0f36a91c6b262c6a4c43adc7682210e3e84c1
OliLobster <sgyr1993@163.com>
2022-10-06 23:35:22 +0800

    load image from internet

--- app/src/main/java/com/ollie/androidtravelblog/BlogDetailsActivity.java
+++ app/src/main/java/com/ollie/androidtravelblog/BlogDetailsActivity.java
@@ -7,17 +7,31 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
 public class BlogDetailsActivity extends AppCompatActivity {
+
+    public static final String IMAGE_URL =
+            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
+                    "3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg";
+    public static final String AVATAR_URL =
+            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
+                    "3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_details);
 
         ImageView imageMain = findViewById(R.id.imageMain);
-        imageMain.setImageResource(R.drawable.sydney_image);
+        Glide.with(this)
+                .load(IMAGE_URL)
+                .into(imageMain);
 
         ImageView imageAvatar = findViewById(R.id.imageAvatar);
-        imageAvatar.setImageResource(R.drawable.avatar);
+        Glide.with(this)
+                .load(AVATAR_URL)
+                .into(imageAvatar);
 
         TextView textTitle = findViewById(R.id.textTitle);
         textTitle.setText("G'day from Sydney");
