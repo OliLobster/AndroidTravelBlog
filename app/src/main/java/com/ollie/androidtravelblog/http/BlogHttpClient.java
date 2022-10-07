6ad3a029b017ffa88a9d8554eaace8f0baa351c5
OliLobster <sgyr1993@163.com>
2022-10-07 10:58:17 +0800

    Update BlogHttpClient.java

--- app/src/main/java/com/ollie/androidtravelblog/http/BlogHttpClient.java
+++ app/src/main/java/com/ollie/androidtravelblog/http/BlogHttpClient.java
@@ -1,4 +1,15 @@
 package com.ollie.androidtravelblog.http;
 
-public class BlogHttpClient {
+public final class BlogHttpClient {
+
+    public static final BlogHttpClient INSTANCE = new BlogHttpClient();
+
+    private static final String BASE_URL =
+            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/";
+    private static final String BLOG_ARTICLES_URL =
+            BASE_URL + "8550ef2064bf14fcf3b9ff322287a2e056c7e153/blog_articles.json";
+
+    private BlogHttpClient() {
+        // to do
+    }
 }
